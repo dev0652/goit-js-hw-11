6c809393fe010060e1a3dcccd0cae930154d532b
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-04-03 18:13:19 +0300

    fixed simplelightbox overlay color override

--- src/index.js
+++ src/index.js
@@ -14,8 +14,6 @@ import {
 import axios from 'axios';
 
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import '../src/css/lightbox-overlay.css';
 
 // ###########################################################################
 
